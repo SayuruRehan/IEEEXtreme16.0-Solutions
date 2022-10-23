4884a68f50bb33b8ff2d035f5954ec631a0547ef
Sayuru Rehan <42121050+SayuruRehan@users.noreply.github.com>
2022-10-23 13:48:22 +0530

    Create ASCII.java

---
+++ ASCIIDancer/ASCII.java
@@ -0,0 +1,53 @@
+// Don't place your source in a package
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+
+// Please name your class Main
+class Main {
+	public static void main (String[] args){
+	    
+	    Scanner say = new Scanner(System.in);
+	    System.out.println("say ");
+	    
+	    if (say == left hand to head){
+	        
+	    }
+	    //starting position
+	    String start = "o\n"
+        + "/"+"|"+"\\n"
+        + "/" +"\\n";
+        
+        //left hand to head
+        String lefttohead = "o)\n"
+        + "/"+"|\n"
+        + "/" +"\\n";
+        
+        //left hand to hip
+        String handtohip = "o\n"
+        + "/"+"|"+">\n"
+        + "/" +"\\n";
+        
+        //right leg in
+        String rightlegin = "o\n"
+        + "/"+"|\n"
+        + "<" +"\\n";
+        
+        //right hand to hip
+        String righttohip = "o\n"
+        + "<"+"|\n"
+        + "/" +"\\n";
+        
+        //left leg in
+        String leftlegin = "o)\n"
+        + "<"+"|\n"
+        + "/" +">\n";
+        
+        
+        System.out.println(start);
+        System.out.println("say" + )
+	
+	}
+}
